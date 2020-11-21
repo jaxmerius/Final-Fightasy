ab52cf8174f5345c22f9a7828ffa1e2197f9aeeb
ElDuderini <Justin_Peters@live.com>
2020-11-20 17:17:51 -0700

    Added more UI and added witch animations with sound

---
+++ Final Fightasy/Assets/Scripts/WitchSFXManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchSFXManager : MonoBehaviour
+{
+    public AudioSource soundPlayer;
+
+    public AudioClip powerAttack;
+    public AudioClip charge;
+    public AudioClip special;
+    public AudioClip guard;
+    public AudioClip jab;
+
+    public void PowerSFXWitch()
+    {
+        soundPlayer.clip = powerAttack;
+        soundPlayer.Play();
+    }
+
+    public void ChargeSFXWitch()
+    {
+        soundPlayer.clip = charge;
+        soundPlayer.Play();
+    }
+
+    public void SpecialSFXWitch()
+    {
+        soundPlayer.clip = special;
+        soundPlayer.Play();
+    }
+
+    public void GuardSFXWitch()
+    {
+        soundPlayer.clip = guard;
+        soundPlayer.Play();
+    }
+
+    public void JabSFXWitch()
+    {
+        soundPlayer.clip = jab;
+        soundPlayer.Play();
+    }
+}
